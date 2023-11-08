053f81c2faf2117b9accb21eef7bd0c78c84f18f
Nariman <onlanovnar@gmail.com>
2023-11-08 17:19:07 +0600

    connected to database

--- app/src/application/db/config.ts
+++ app/src/application/db/config.ts
@@ -3,7 +3,6 @@ import { Dialect, Sequelize } from 'sequelize';
 const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
   dialect: process.env.DB_USER as Dialect,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT as unknown as number,
 });
 
 export default sequelize;
